1e2d3101420c7de07169ff6870de5799571e9ac3
Dawid <dmaruszczyk@yahoo.de>
2018-05-14 08:33:27 +0200

    new tests for Movie

--- code/refactoring/src/test/java/myproject/MovieTest.java
+++ code/refactoring/src/test/java/myproject/MovieTest.java
@@ -1,4 +1,21 @@
 package test.java.myproject;
 
+import main.java.myproject.Movie;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class MovieTest {
+
+    Movie movie = new Movie("test",30);
+
+    @Test
+    public void testMovieTitle() {
+        assertEquals(movie.getTitle(),"test");
+    }
+
+    @Test
+    public void testMoviePrice() {
+        assertEquals(movie.getPriceCode(),30);
+    }
 }
