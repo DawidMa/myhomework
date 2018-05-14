4ff766175391e2f6c6f0168ae583271f2e75dda2
Dawid <dmaruszczyk@yahoo.de>
2018-05-14 08:36:04 +0200

    new tests for Rental

--- code/refactoring/src/test/java/myproject/RentalTest.java
+++ code/refactoring/src/test/java/myproject/RentalTest.java
@@ -1,5 +1,23 @@
 package test.java.myproject;
 
+import main.java.myproject.Movie;
+import main.java.myproject.Rental;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RentalTest {
 
+    Movie movie = new Movie("test",30);
+    Rental rental = new Rental(movie,5);
+
+    @Test
+    public void testRentalMovie() {
+        assertEquals(rental.getMovie().getTitle(),"test");
+    }
+
+    @Test
+    public void testRentalPrice() {
+        assertEquals(rental.getMovie().getPriceCode(),30);
+    }
 }
